313961586bc96d1eedd6c01ec67c50f60f000b62
Arlinton Ramón Mayorga Marin <161267080+RamonAPIAlchemist@users.noreply.github.com>
2025-11-15 16:11:28 -0600

    Fix missing FIREBASE_CLIENT_X509_CERT_URL environment variable

--- inicio.py
+++ inicio.py
@@ -8,6 +8,7 @@ app = Flask(__name__, template_folder="templates")
 app.secret_key = 'tu_clave_secreta_aqui'  # ¡IMPORTANTE! Agrega una clave secreta
 
 # ----------------- CONFIGURACIÓN FIREBASE -----------------
+# ----------------- CONFIGURACIÓN FIREBASE -----------------
 try:
     print("🚀 INICIANDO FIREBASE DESDE VARIABLES DE ENTORNO...")
     
@@ -22,14 +23,14 @@ try:
     firebase_config = {
         "type": "service_account",
         "project_id": os.environ['FIREBASE_PROJECT_ID'],
-        "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
+        "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID', '4de59097dcedb9abd1ea52f31e45a24ca4d00770'),
         "private_key": os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
         "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
-        "client_id": os.environ['FIREBASE_CLIENT_ID'],
+        "client_id": os.environ.get('FIREBASE_CLIENT_ID', '110208283106507006922'),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ['FIREBASE_CLIENT_X509_CERT_URL'],
+        "client_x509_cert_url": os.environ.get('FIREBASE_CLIENT_X509_CERT_URL', 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40pay-nic.iam.gserviceaccount.com'),
         "universe_domain": "googleapis.com"
     }
 
